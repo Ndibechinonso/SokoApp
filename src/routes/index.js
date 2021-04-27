8877de64a4644c5d38fb296be94cab34fd159711
Ndibechinonso <edwardndibe@gmail.com>
2021-04-27 20:15:01 +0100

    final  commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,20 @@
 import * as React from 'react'
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Homepage from '../components/Homepage'
 import EmptyCart from '../components/EmptyCart'
 import Account from '../components/Account'
 import ProductDetails from '../components/ProductDetails'
 const Routes = () => (
+  <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Homepage} />
     <Route exact path='/cart' component={EmptyCart} />
     <Route exact path='/account' component={Account} />
     <Route exact path='/productDetails' component={ProductDetails} />
+    <Route exact path="/" component={Homepage} />
       <Route path="*" component={() => <div>Not found</div>} />
     </Switch>
+    </BrowserRouter>
   );
 
     export default Routes
